ed15add6e950f5a05a6764919c09f60c42188685
Eduardo Junior <edujrrib@gmail.com>
2018-02-02 14:24:06 -0200

    Change lattice settings

--- project/config.R
+++ project/config.R
@@ -1,57 +1,21 @@
-##----------------------------------------------------------------------
-## Report settings
-library(knitr)
-opts_chunk$set(
-    warning = FALSE,
-    message = FALSE,
-    fig.width = 9,
-    fig.height = 4,
-    dev.args = list(family = "Palatino"))
-
-##----------------------------------------------------------------------
-## Load the lattice packages and customize outputs graphics
+#-----------------------------------------------------------------------
+# Load the lattice packages and customize outputs graphics
 library(lattice)
 library(latticeExtra)
 library(grid)
 
-## http://www.magesblog.com/2013/04/how-to-change-alpha-value-of-colours-in.html
-add.alpha <- function(col, alpha = 1){
-    apply(sapply(col, col2rgb)/255, 2,
-          function(x) rgb(x[1], x[2], x[3], alpha = alpha))
-}
-
-## Define Colors
-mycol <- c(1, "#377EB8", "#E41A1C", "#4DAF4A",
-           "#ff00ff", "#FF7F00", "#984EA3", "#FFFF33", "#808080")
-myreg <- colorRampPalette(c(mycol[3],  "gray90", mycol[2]))(100)
-
-## Trellis graphical style.
-ps <- list(
-    superpose.symbol = list(
-        col = mycol, pch = 1,
-        fill = add.alpha(mycol, alpha = 0.4)),
-    box.rectangle = list(col = 1, fill = c("gray70")),
+trellis.par.set(canonical.theme(color = FALSE))
+ac <- list(pad1 = 0.5, pad2 = 0.65, tck = 0.5)
+trellis.par.set(
+    reference.line = list(col = "gray90"),
+    strip.background = list(col = c("gray85", "gray75")),
+    box.rectangle = list(col = 1, fill = c("gray80")),
     box.umbrella = list(col = 1, lty = 1),
-    box.dot = list(pch = "|"),
-    dot.symbol = list(col = 1, pch = 19),
-    dot.line = list(col = "gray50", lty = 3),
-    plot.symbol = list(col = 1),
-    plot.line = list(col = 1),
-    plot.polygon = list(col = "gray95"),
-    superpose.line = list(col = mycol, lty = 1),
-    superpose.polygon = list(col = mycol),
-    strip.background = list(col = c("gray90", "gray70")),
-    regions = list(col = myreg),
     par.sub.text = list(
         font = 1, just = "left", cex = 0.9,
-        x = grid::unit(10, "mm"))
-    )
-
-trellis.par.set(ps)
-
-## Alternative settings
-ac <- list(pad1 = 0.5, pad2 = 0.5, tck = 0.5)
-ps2 <- list(
+        x = grid::unit(10, "mm")),
+    box.dot = list(pch = "|"),
+    plot.polygon = list(col = "gray90"),
     layout.widths = list(
         left.padding = 0.25,
         right.padding = -1,
@@ -65,3 +29,8 @@ ps2 <- list(
         bottom = ac, top = ac,
         left = ac, right = ac)
 )
+
+# Settings for legends
+ltys <- trellis.par.get("superpose.line")$lty
+pchs <- trellis.par.get("superpose.symbol")$pch
+cols <- trellis.par.get("superpose.polygon")$col
