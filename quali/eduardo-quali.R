69858e392e76f1714406f6656e752030cb6eeba4
Eduardo Junior <edujrrib@gmail.com>
2019-01-30 16:25:53 -0200

    Add other files to take qualifying exam

--- quali/eduardo-quali.R
+++ quali/eduardo-quali.R
@@ -47,9 +47,12 @@ xyplot(y ~ x | caso,
                     labels = rep("", length(pretty(coords$x))))
        ),
        strip = strip.custom(
-           factor.levels = c("Equidispersão",
-                             "Superdispersão",
-                             "Subdispersão")),
+           factor.levels = c("Equidispersion",
+                             "Overdispersion",
+                             "Underdispersion")),
+           # factor.levels = c("Equidispersão",
+           #                   "Superdispersão",
+           #                   "Subdispersão")),
        panel = function(x, y, ...) {
            panel.grid(h = 10, v = 10, col = col, lty = 2)
            panel.xyplot(x, y, ...)
@@ -79,6 +82,48 @@ xyplot(ncap ~ des | est,
        spread = 0.05,
        panel = cmpreg::panel.beeswarm)
 
+## ---- desc-cotton2
+cotton <- read_delim("../data/cotton.txt", delim = "\t") %>%
+    mutate(est = fct_inorder(factor(est)))
+levels(cotton$est) <- c("vegetative",
+                        "flower bud",
+                        "blossom",
+                        "fig",
+                        "cotton boll")
+cotton
+
+cotton_summary <- cotton %>%
+    group_by(des, est) %>%
+    summarise(me = mean(ncap), va = var(ncap))
+
+xy1 <- xyplot(ncap ~ des | est,
+              data = cotton,
+              layout = c(NA, 3),
+              as.table = TRUE,
+              type = c("p", "smooth", "g"),
+              xlab = "Artificial defoliation level",
+              ylab = "Number of bolls produced",
+              sub = "(a)",
+              spread = 0.05,
+              panel = cmpreg::panel.beeswarm)
+
+lim <- extendrange(unlist(cotton_summary[, c("me", "va")]))
+xy2 <- xyplot(va ~ me,
+              xlim = lim,
+              ylim = lim,
+              type = c("g", "p", "r"),
+              sub = "(b)",
+              xlab = "Sample mean",
+              ylab = "Sample variance",
+              data = cotton_summary,
+              panel = function(...) {
+                  panel.xyplot(...)
+                  panel.abline(a = 0, b = 1, lty = 2)
+              })
+
+print(xy1, split = c(1, 1, 2, 1), more = TRUE)
+print(xy2, split = c(2, 1, 2, 1), more = FALSE)
+
 #-----------------------------------------------------------------------
 # Descriptive analysis of the soybeans data set
 ## ---- desc-soya
@@ -105,6 +150,60 @@ xyplot(ngra + nvag ~ K | umid,
        layout = c(NA, 1),
        strip = strip.custom(factor.levels = fvls))
 
+## ---- desc-soya2
+
+# Load data
+soybean <- read_delim("../data/soybean.txt", delim = "\t") %>%
+    mutate(umid = factor(umid)) %>%
+    slice(-74)
+soybean
+
+soybean_summary <-
+    soybean %>%
+    gather(key = "var", value = "count", ngra, nvag) %>%
+    group_by(K, umid, var) %>%
+    summarise(me = mean(count), va = var(count))
+
+fvls <- paste0("Soil moisture : ", levels(soybean$umid), "%")
+xy1 <- xyplot(ngra + nvag ~ K | umid,
+              data = soybean,
+              xlab = "Potassium fertilization level",
+              ylab = "Count",
+              sub = "(a)",
+              type = c("p", "g", "a"),
+              jitter.x = TRUE,
+              layout = c(NA, 2),
+              as.table = TRUE,
+              key = list(
+                  type = "b",
+                  divide = 1,
+                  lines = list(pch = pchs[1:2], lty = ltys[1:2]),
+                  text = list(c("Number of grains",
+                                "Number of viable pods"))),
+              strip = strip.custom(factor.levels = fvls))
+
+lim <- extendrange(unlist(soybean_summary[, c("me", "va")]))
+xy2 <- xyplot(va ~ me | var,
+              xlim = lim,
+              ylim = lim,
+              type = c("g", "p", "r"),
+              sub = "(b)",
+              xlab = "Sample mean",
+              ylab = "Sample variance",
+              data = soybean_summary,
+              layout = c(NA, 2),
+              strip = strip.custom(
+                  factor.levels = c("Number of grains",
+                                    "Number of viable pods")
+              ),
+              panel = function(...) {
+                  panel.xyplot(...)
+                  panel.abline(a = 0, b = 1, lty = 2)
+              })
+
+print(xy1, position = c(0, 0, 0.6, 1), more = TRUE)
+print(xy2, position = c(0.6, 0, 1, 0.95), more = FALSE)
+
 #-----------------------------------------------------------------------
 # Descriptive analysis of the epilepsy data set
 
@@ -127,6 +226,35 @@ xyplot(nseizw ~ week | trt,
        scales = list(x = "free"),
        xlab = "Tempo (semanas)",
        ylab = "Número de convulsões",
+       # key = list(columns = 2,
+       #            text = list(paste(c("Average", "Median"),
+       #                               "profile")),
+       #            lines = list(lty = c(1, 2), lwd = 2)),
+       data = epilepsy) +
+    xyplot(ave + med ~ week | trt,
+           type = c("l"),
+           lwd = 2,
+           scales = list(x = "free"),
+           data = epilepsy_summary)
+
+## ---- desc-epilepsy2
+epilepsy <- read_delim("../data/epilepsy.csv", delim = ";") %>%
+    mutate(trt = factor(trt, labels = c("Placebo", "Treatment")),
+           sex = factor(sex),
+           race = factor(race),
+           date0 = lubridate::dmy(date0)) %>%
+    rename("week" = studyweek)
+
+epilepsy_summary <- epilepsy %>% group_by(trt, week) %>%
+    summarise(ave = mean(nseizw), med = median(nseizw))
+xyplot(nseizw ~ week | trt,
+       groups = id,
+       lty = 1,
+       col = "gray70",
+       type = c("l", "g"),
+       scales = list(x = "free"),
+       xlab = "Time (weeks)",
+       ylab = "Number of seizures",
        # key = list(columns = 2,
        #            text = list(paste(c("Average", "Median"),
        #                               "profile")),
@@ -222,6 +350,71 @@ xy1 <- useOuterStrips(
            type = c("g", "l"),
            xlab = "Tempo de infestação (dias)",
            ylab = "Número de ninfas de mosca-branca",
+           sub = "(a)",
+           col = "gray50",
+           lty = 1,
+           data = whitefly) +
+    xyplot(ave_ninf + ave_nmed + ave_nsup ~ dias | cult,
+           outer = TRUE,
+           type = c("l"),
+           lwd = 2,
+           data = whitefly_summary),
+    strip.left = strip.custom(factor.levels = flvs)
+)
+
+aux <- whitefly_summary %>%
+    gather(key = posa, value = ave, ave_nsup, ave_nmed, ave_ninf) %>%
+    gather(key = posb, value = var, var_nsup, var_nmed, var_ninf) %>%
+    mutate(lave = log(ave), lvar = log(var)) %>%
+    ungroup()
+index <- apply(aux[, 7:8], 1L, function(x) any(is.infinite(x)))
+
+lim <- extendrange(unlist(aux[!index, c("lave", "lvar")]))
+xy2 <- xyplot(lvar ~ lave | posa,
+              data = aux[!index, ],
+              type = c("g", "p", "r"),
+              # xlab = expression(log(bar(y))),
+              # ylab.right = expression(log(s^2)),
+              xlab = list("média", col = "white"),
+              ylab = "",
+              sub = "(b)",
+              layout = c(NA, 3),
+              strip = FALSE,
+              ylim = lim,
+              xlim = lim,
+              strip.left = strip.custom(
+                  factor.levels = flvs,
+                  bg = "gray75"),
+              panel = function(x, y, ...) {
+                  panel.xyplot(x, y, ...)
+                  panel.abline(0, 1, lty = 2)
+              })
+
+print(xy1, position = c(0, 0, 0.77, 1), more = TRUE)
+print(xy2, position = c(0.75, 0, 1, 0.95), more = FALSE)
+
+## ---- desc-whitefly2
+
+# Load data set
+whitefly <- read_delim("../data/whitefly.txt", delim = "\t") %>%
+    filter(grepl("BRS", x = cult)) %>%
+    mutate(cult  = fct_inorder(factor(cult)),
+           bloco = fct_inorder(factor(bloco)))
+
+# Visualize profiles
+whitefly_summary <- whitefly %>% group_by(cult, dias) %>%
+    summarise(ave_nsup = mean(nsup), var_nsup = var(nsup),
+              ave_nmed = mean(nmed), var_nmed = var(nmed),
+              ave_ninf = mean(ninf), var_ninf = var(ninf))
+
+flvs <- paste(c("lower", "middle", "upper"), "third")
+xy1 <- useOuterStrips(
+    xyplot(ninf + nmed + nsup ~ dias | cult,
+           outer = TRUE,
+           groups = bloco,
+           type = c("g", "l"),
+           xlab = "Time of infestation (days)",
+           ylab = "Number of whitefly nymphs",
            sub = "(a)",
            col = "gray50",
            lty = 1,
